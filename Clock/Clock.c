ce7a0cc7f59716a57c5a54e8ede9c5111e2c6687
godisblack <163236412+godisblaack@users.noreply.github.com>
2024-06-01 13:54:44 +0530

    Added a date function to display date

--- Clock/Clock.c
+++ Clock/Clock.c
@@ -21,14 +21,27 @@ void fillTime12(char *buffer, size_t size) {
   strftime(buffer, size, "%I:%M:%S %p", currentTime);
 }
 
+void fillDate(char *buffer, size_t size) {
+  time_t rawTime;
+  struct tm *currentTime;
+
+  time(&rawTime);
+  currentTime = localtime(&rawTime);
+
+  strftime(buffer, 100, "%A %B %d %Y", currentTime);
+}
+
 int main() {
-  char time[50];
+  char time[50], date[50];
 
   fillTime24(time, sizeof(time));
   printf("\nCurrent Time: %s\n", time);
 
   fillTime12(time, sizeof(time));
-  printf("\nCurrent Time: %s\n\n", time);
+  printf("\nCurrent Time: %s\n", time);
+
+  fillDate(date, sizeof(date));
+  printf("\nDate: %s\n\n", date);
 
   return 0;
 }
